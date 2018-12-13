eab85051f61725c64dda4b8f4189cebf3316549c
Handora <qcdsr970209@gmail.com>
2018-12-13 22:35:49 +0800

    update cmakelist not depend on gitsubmodule

--- src/parser/parser.cc
+++ src/parser/parser.cc
@@ -8,6 +8,7 @@
 
 #include <parser/parser.h>
 #include <client.pb.h>
+#include <glog/logging.h>
 
 namespace ctgfs {
 namespace parser {
@@ -18,24 +19,28 @@ bool Parser::ParseFromInput(const std::string& input, Command& command) {
                         istream_iterator<string>{}};
 
   if (tokens[0].compare("mkdir") == 0) {
+    LOG(INFO) << "Get Token " << tokens[0] << std::endl;
     command.type = kCREATE_DIR;    
     if (tokens.size() != 2) {
       return false;
     }
     command.path = tokens[1];
   } else if (tokens[0].compare("rmdir") == 0) {
+    LOG(INFO) << "Get Token " << tokens[0] << std::endl;
     command.type = kREMOVE_DIR;
     if (tokens.size() != 2) {
       return false;
     }
     command.path = tokens[1];
   } else if (tokens[0].compare("read") == 0) {
+    LOG(INFO) << "Get Token " << tokens[0] << std::endl;
     command.type = kREAD_FILE;
     if (tokens.size() != 2) {
       return false;
     }
     command.path = tokens[1];
   } else if (tokens[1].compare("write") == 0) {
+    LOG(INFO) << "Get Token " << tokens[0] << std::endl;
     command.type = kWRITE_FILE;
     if (tokens.size() != 3) {
       return false;
@@ -43,11 +48,14 @@ bool Parser::ParseFromInput(const std::string& input, Command& command) {
     command.path = tokens[1];
     command.value = tokens[2];
   } else if (tokens[1].compare("rm") == 0) {
+    LOG(INFO) << "Get Token " << tokens[0] << std::endl;
     command.type = kREMOVE_FILE;
     if (tokens.size() != 2) {
       return false;
     }
     command.path = tokens[1];
+  } else {
+    return false;
   }
 
   return true;
