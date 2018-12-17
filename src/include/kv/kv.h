99a71127d881a822f185914630a4ab0348966506
Handora <qcdsr970209@gmail.com>
2018-12-18 00:03:19 +0800

    add kv interface

---
+++ src/include/kv/kv.h
@@ -0,0 +1,26 @@
+// Authors: Chen Qian(qcdsr970209@gmail.com)
+
+#pragma once
+#include <string>
+
+namespace ctgfs {
+namespace kv {
+
+// The kv store interface that you can use it to store or get key and
+// value.
+class KV {
+ public:
+  // Given the key reference and value reference which you want to
+  // put into the kv store, it will return bool to indicate success 
+  // or not.
+  virtual bool Put(const std::string& key, const std::string& value) = 0;
+
+  // Given the key reference which you want to search within the kv
+  // store, it will return bool to indicate sucess or not. If success,
+  // the value will be the corresponding value. Otherwise, don't use 
+  // it.
+  virtual bool Get(const std::string& key, std::string& value) = 0;
+};
+
+} // namespace kv
+} // namespace ctgfs
---
+++ src/kv/raft_kv.cc
@@ -0,0 +1,19 @@
+// Authors: Chen Qian(qcdsr970209@gmail.com)
+
+#include <kv/raft_kv.h>
+
+namespace ctgfs {
+namespace kv {
+
+bool RaftKV::Put(const std::string& key, const std::string& value) {
+  // TODO(Handora): Fill in function
+  return true;
+}
+
+bool RaftKV::Get(const std::string& key, std::string& value) {
+  // TODO(Handora): Fill in function
+  return true;
+}
+
+}
+}
