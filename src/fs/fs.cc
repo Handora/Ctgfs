c1b13b99e802115536523838531ff8fb12ebc2a8
Prgwf <peter.wfgu@gmail.com>
2018-12-19 22:48:00 +0800

    FS interface update.

--- src/fs/fs.cc
+++ src/fs/fs.cc
@@ -4,36 +4,47 @@
 
 #include <string>
 #include <vector>
+#include <memory>
 
-#include "fs/fs.h"
+#include <fs/fs.h>
 
 namespace ctgfs {
 namespace fs {
 
 bool FileSystem::CreateDir(const std::string& path) {
   // todo 
+  return true;
 }
 
 bool FileSystem::ReadDir(const std::string& path, std::vector<std::string>& children) const {
   // todo    
+  return true;
 }
 
 bool FileSystem::RemoveDir(const std::string& path) {
   // todo 
+  return true;
 }
 
 bool FileSystem::WriteFile(const std::string& path, const std::string& content) {
   // todo 
+  return true;
 }
 
 bool FileSystem::ReadFile(const std::string& path, std::string& content) const {
   // todo 
+  return true;
 }
 
 bool FileSystem::RemoveFile(const std::string& path) {
   // todo
+  return true;
 }
 
+bool FileSystem::ChangeDir(const std::string& path, std::string& key) const {
+  // todo  
+  return true;
+}
 
 } // namespace fs
 } // namespace ctgfs
