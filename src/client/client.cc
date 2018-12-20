1dadd6fe620ff2d531f2af10ceedd3ca937c06ff
OneDay_ <ltang970618@gmail.com>
2018-12-21 03:02:29 +0800

    code format

--- src/client/client.cc
+++ src/client/client.cc
@@ -2,133 +2,123 @@
 * author: OneDay_(ltang970618@gmail.com)
 **/
 
-
 #include <client/client.h>
-#include <iostream>
 #include <parser/parser.h>
-#include <string>
 #include <algorithm>
+#include <iostream>
+#include <string>
 
-namespace ctgfs{
-namespace client{
-  Client::Client(){}
+namespace ctgfs {
+namespace client {
+Client::Client() {}
 
-  Client::Client(const std::string& input) {
-    Client(input,DEFAULT_SERVER_ADDR);
-  }
+Client::Client(const std::string& input) { Client(input, DEFAULT_SERVER_ADDR); }
 
-  Client::Client(const std::string& input,const std::string& ip, const int port)
-    : command_input_(input)	{
-    initChannel(ip,port);
-  }
-
-  Client::Client(const std::string& input,const std::string& addr)
+Client::Client(const std::string& input, const std::string& ip, const int port)
     : command_input_(input) {
-    initChannel(addr);
-  }
+  initChannel(ip, port);
+}
 
-  bool Client::StartClient() {
-    return parserInput();	
-  }
+Client::Client(const std::string& input, const std::string& addr)
+    : command_input_(input) {
+  initChannel(addr);
+}
 
-  void Client::initChannel(const std::string& addr) {
-    auto split_pos = std::find_if(addr.begin(),addr.end(),
-      [](char x) { 
-        return (x == ':');
-      }
-    );	
-    std::string ip = std::string(addr.begin(),split_pos);
-    std::string port_str = std::string(split_pos + 1,addr.end());
-    int port = atoi(port_str.c_str());
-    initChannel(ip,port);
-  }
+bool Client::StartClient() { return parserInput(); }
 
-  void Client::initChannel(const std::string& ip, const int port) {
-    client_response_ptr_ = std::make_shared<ClientKVResponse>();
-    brpc::ChannelOptions options;
-    client_channel_.Init(ip.c_str(),port,&options);
-  }
+void Client::initChannel(const std::string& addr) {
+  auto split_pos =
+      std::find_if(addr.begin(), addr.end(), [](char x) { return (x == ':'); });
+  std::string ip = std::string(addr.begin(), split_pos);
+  std::string port_str = std::string(split_pos + 1, addr.end());
+  int port = atoi(port_str.c_str());
+  initChannel(ip, port);
+}
 
-  bool Client::parserInput() {
-    client_request_ptr_ = std::make_shared<ClientKVRequest>();
-    parser::Parser parser;
-    if(!parser.ParseFromInput(command_input_, (*client_request_ptr_.get()))) {
-      debugErrorParserInput(true, "Parse Input Command Error!");
-      return false;
-    }
-    debugErrorParserInput(false,"Parse Successfully, Starting Connecting Master");
-    return connectToMaster();
-  }
+void Client::initChannel(const std::string& ip, const int port) {
+  client_response_ptr_ = std::make_shared<ClientKVResponse>();
+  brpc::ChannelOptions options;
+  client_channel_.Init(ip.c_str(), port, &options);
+}
 
-  bool Client::connectToMaster() {
-    debugErrorConnectToMaster(false, "Master Connect Successfully\nStarting Communicating");
-    return askKV();
+bool Client::parserInput() {
+  client_request_ptr_ = std::make_shared<ClientKVRequest>();
+  parser::Parser parser;
+  if (!parser.ParseFromInput(command_input_, (*client_request_ptr_.get()))) {
+    debugErrorParserInput(true, "Parse Input Command Error!");
+    return false;
   }
+  debugErrorParserInput(false,
+                        "Parse Successfully, Starting Connecting Master");
+  return connectToMaster();
+}
 
-  bool Client::askKV() {
-    MasterService_Stub stub(&client_channel_);
-    brpc::Controller ctrl;
-    stub.ClientAskForKV(&ctrl, client_request_ptr_.get(), 
-        client_response_ptr_.get(), NULL);
-    if(ctrl.Failed()) {
-      debugErrorAskKV(true, "Fail to get target address!");	
-      return false;	
-    }
-    else {
-      debugErrorAskKV(false, "Successfully!Start connecting target!");
-      return connectToKV();
-    }
+bool Client::connectToMaster() {
+  debugErrorConnectToMaster(
+      false, "Master Connect Successfully\nStarting Communicating");
+  return askKV();
+}
+
+bool Client::askKV() {
+  MasterService_Stub stub(&client_channel_);
+  brpc::Controller ctrl;
+  stub.ClientAskForKV(&ctrl, client_request_ptr_.get(),
+                      client_response_ptr_.get(), NULL);
+  if (ctrl.Failed()) {
+    debugErrorAskKV(true, "Fail to get target address!");
+    return false;
+  } else {
+    debugErrorAskKV(false, "Successfully!Start connecting target!");
+    return connectToKV();
   }
+}
 
-  bool Client::connectCallback() {
+bool Client::connectCallback() {}
 
+bool Client::connectToKV() {
+  // connect kv
+}
+
+bool Client::doCommand() {
+  // do command
+}
+
+void Client::debugErrorParserInput(bool is_error, const char* str) {
+  debugErrorParserInput(is_error, std::string(str));
+}
+
+void Client::debugErrorParserInput(bool is_error,
+                                   const std::string& error_str) {
+  if (is_error) {
+    std::cout << error_str << std::endl;
+  } else {
+    std::cout << error_str << std::endl;
   }
+}
 
-  bool Client::connectToKV() {
-    // connect kv
-  }
+void Client::debugErrorConnectToMaster(bool is_error, const char* str) {
+  debugErrorConnectToMaster(is_error, std::string(str));
+}
 
-  bool Client::doCommand() {
-    // do command
+void Client::debugErrorConnectToMaster(bool is_error,
+                                       const std::string& error_str) {
+  if (is_error) {
+    std::cout << error_str << std::endl;
+  } else {
+    std::cout << error_str << std::endl;
   }
+}
 
-  void Client::debugErrorParserInput(bool is_error,const char* str) {
-    debugErrorParserInput(is_error, std::string(str));
-  }
+void Client::debugErrorAskKV(bool is_error, const char* str) {
+  debugErrorAskKV(is_error, std::string(str));
+}
 
-  void Client::debugErrorParserInput(bool is_error,const std::string& error_str) {
-    if(is_error) {
-      std::cout << error_str << std::endl;
-    }
-    else {
-      std::cout << error_str << std::endl;
-    }
+void Client::debugErrorAskKV(bool is_error, const std::string& error_str) {
+  if (is_error) {
+    std::cout << error_str << std::endl;
+  } else {
+    std::cout << error_str << std::endl;
   }
-
-  void Client::debugErrorConnectToMaster(bool is_error,const char* str) {
-    debugErrorConnectToMaster(is_error,std::string(str));
-  }
-
-  void Client::debugErrorConnectToMaster(bool is_error,const std::string& error_str) {
-    if(is_error) {
-      std::cout << error_str << std::endl;
-    }
-    else {
-      std::cout << error_str << std::endl;
-    }
-  }
-
-  void Client::debugErrorAskKV(bool is_error,const char* str) {
-    debugErrorAskKV(is_error,std::string(str));
-  }
-
-  void Client::debugErrorAskKV(bool is_error,const std::string& error_str) {
-    if(is_error) {
-      std::cout << error_str << std::endl;
-    }
-    else {
-      std::cout << error_str << std::endl;
-    }
-  }
+}
 }
 }
--- src/include/client/client.h
+++ src/include/client/client.h
@@ -2,46 +2,47 @@
 * author: OneDay_(ltang970618@gmail.com)
 **/
 #pragma once
+#include <brpc/channel.h>
+#include <butil/time.h>
 #include <client.pb.h>
+#include <gflags/gflags.h>
 #include <memory>
 #include <string>
-#include <gflags/gflags.h>
-#include <butil/time.h>
-#include <brpc/channel.h>
 
 namespace ctgfs {
 namespace client {
-	std::string DEFAULT_SERVER_ADDR = "127.0.0.1:1234";
-  class Client {
-   public: 
-    Client();
-    Client(const std::string& input);
-    Client(const std::string& input,const std::string& ip,const int port);
-    Client(const std::string& input,const std::string& addr);
-    bool StartClient();
-   private:
-    brpc::Channel client_channel_;
-    std::shared_ptr<ClientKVRequest> client_request_ptr_;
-    std::shared_ptr<ClientKVResponse> client_response_ptr_;
-    const std::string command_input_;
-    // init channel call this function before every connect
-    void initChannel(const std::string& addr);
-    void initChannel(const std::string& ip, const int port);
-    bool parserInput();
-    bool connectToMaster();
-    // to get kv addr
-    bool askKV();
-    // wait to imporve
-    bool connectCallback();	
-    bool connectToKV();
-    bool doCommand();
-    // for debug and log
-    void debugErrorParserInput(bool is_error,const char*);
-    void debugErrorParserInput(bool is_error,const std::string& error_str = "");
-    void debugErrorConnectToMaster(bool,const char*);
-    void debugErrorConnectToMaster(bool,const std::string& error_str = "");
-    void debugErrorAskKV(bool, const char*);
-    void debugErrorAskKV(bool, const std::string&);
-  };
-} // namespace client
-} // namespace ctgfs
+std::string DEFAULT_SERVER_ADDR = "127.0.0.1:1234";
+class Client {
+ public:
+  Client();
+  Client(const std::string& input);
+  Client(const std::string& input, const std::string& ip, const int port);
+  Client(const std::string& input, const std::string& addr);
+  bool StartClient();
+
+ private:
+  brpc::Channel client_channel_;
+  std::shared_ptr<ClientKVRequest> client_request_ptr_;
+  std::shared_ptr<ClientKVResponse> client_response_ptr_;
+  const std::string command_input_;
+  // init channel call this function before every connect
+  void initChannel(const std::string& addr);
+  void initChannel(const std::string& ip, const int port);
+  bool parserInput();
+  bool connectToMaster();
+  // to get kv addr
+  bool askKV();
+  // wait to imporve
+  bool connectCallback();
+  bool connectToKV();
+  bool doCommand();
+  // for debug and log
+  void debugErrorParserInput(bool is_error, const char*);
+  void debugErrorParserInput(bool is_error, const std::string& error_str = "");
+  void debugErrorConnectToMaster(bool, const char*);
+  void debugErrorConnectToMaster(bool, const std::string& error_str = "");
+  void debugErrorAskKV(bool, const char*);
+  void debugErrorAskKV(bool, const std::string&);
+};
+}  // namespace client
+}  // namespace ctgfs
