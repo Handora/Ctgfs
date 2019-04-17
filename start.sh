0c0bae1118c228f5375613b73fb75799b9d1f892
Handora <qcdsr970209@gmail.com>
2019-04-17 15:40:37 +0800

    add client

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 echo "opening lock server on port 55555..."
-build/src/master/lock_server 55555 &
+build/src/master/lock_server 55555 > ctgfs_lock_server.log 2>&1 &
 echo "open ok"
 echo "opening extent server on port 55556..."
-build/src/fs/extent_server 55556 &
+build/src/fs/extent_server 55556 > ctgfs_server.log 2>&1 &
 echo "open ok"
+echo "mount to ctgfs dir"
+mkdir -p ctgfs
+build/src/client/yfs_client ctgfs 55556 55555 > ctgfs_client.log 2>&1 &
--- stop.sh
+++ stop.sh
@@ -2,4 +2,5 @@
 
 ps | grep lock_server | awk '{print $1}' | xargs kill -9
 ps | grep extent_server | awk '{print $1}' | xargs kill -9
+ps | grep yfs_client | awk '{print $1}' | xargs kill -9
 echo "stop ok"
