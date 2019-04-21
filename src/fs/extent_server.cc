bc42e115ce8a45dcaee3f7f61bb8316a42e02144
Prgwf <peter.wfgu@gmail.com>
2019-04-22 00:12:18 +0800

    add info collector

--- src/fs/extent_server.cc
+++ src/fs/extent_server.cc
@@ -1,17 +1,54 @@
 // the extent server implementation
 
 #include "extent_server.h"
+#include "info_detector.h"
+#include "master.pb.h"
+#include "fs/heart_beat_sender.h"
 #include <sstream>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <thread>
+#include <brpc/server.h>
+#include <brpc/stream.h>
+#include <brpc/channel.h>
 
 extent_server::extent_server() {
   VERIFY(pthread_mutex_init(&server_mu_, 0) == 0);
   int res;
   VERIFY(put(0x00000001, "", res) == extent_protocol::OK);
+
+  
+  InfoDetector* info = InfoDetector::detector();
+  // auto p_heart_beat_info = std::make_shared<ctgfs::heart_beat::HeartBeatInfo>(
+  //   (ctgfs::heart_beat::HeartBeatInfo){
+  //     ctgfs::HeartBeatMessageRequest_HeartBeatType::HeartBeatMessageRequest_HeartBeatType_kInfoUpdate,
+  //     std::string("127.0.0.1:1235"),
+  //     info->get().file_num, 
+  //     info->get().disk_usage
+  //   });
+  auto p_heart_beat_info = std::make_shared<ctgfs::heart_beat::HeartBeatInfo>();
+  std::string addr = std::string("127.0.0.1:1235");
+  ctgfs::heart_beat::HeartBeatSender sender(
+    addr, p_heart_beat_info);
+  sender.SendHeartBeat();
+  // std::thread t_heart_beat([&]() {
+  //   while (true) {
+  //     auto p_heart_beat_info = std::make_shared<ctgfs::heart_beat::HeartBeatInfo>(
+  //       (ctgfs::heart_beat::HeartBeatInfo){
+  //         ctgfs::HeartBeatMessageRequest_HeartBeatType::HeartBeatMessageRequest_HeartBeatType_kInfoUpdate,
+  //         std::string("127.0.0.1:1235"),
+  //         info->get().file_num, 
+  //         info->get().disk_usage
+  //       });
+  //     sender.SetHeartBeatInfo(p_heart_beat_info);
+  //     sender.SendHeartBeat();
+  //     std::this_thread::sleep_for(std::chrono::seconds(3));
+  //   }
+  // });
+  // t_heart_beat.detach();
 }
 
 extent_server::~extent_server() {}
@@ -31,6 +68,13 @@ int extent_server::put(extent_protocol::extentid_t id, std::string buf, int &)
   
   extent *value = new extent(now, now, now, buf);
   extent_map_.insert(std::pair<extent_protocol::extentid_t, extent*>(id, value));
+
+  InfoDetector* info = InfoDetector::detector();
+  fs_info attr = info->get();
+  attr.file_num++;
+  attr.disk_usage += buf.size();
+  info->set(attr);
+
   return extent_protocol::OK;
 }
 
@@ -84,6 +128,11 @@ int extent_server::setattr(extent_protocol::extentid_t id, extent_protocol::attr
       it->second->content.resize(new_size);
     }
 
+    InfoDetector* info = InfoDetector::detector();
+    fs_info attr = info->get();
+    attr.disk_usage += new_size - old_size;
+    info->set(attr);
+
     return extent_protocol::OK;
   }
 
@@ -95,8 +144,16 @@ int extent_server::remove(extent_protocol::extentid_t id, int &)
   ScopedLock lm(&server_mu_);
   std::map<extent_protocol::extentid_t, extent*>::iterator it = extent_map_.find(id);
   if (it != extent_map_.end()) {
+    int file_size = it->second->content.size();
+
     delete it->second;
     extent_map_.erase(it);
+
+    InfoDetector* info = InfoDetector::detector();
+    fs_info attr = info->get();
+    attr.file_num--;
+    attr.disk_usage -= file_size;
+    info->set(attr);
 
     return extent_protocol::OK;
   }
