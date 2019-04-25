b8a9742ae309aac2ceb5798e81bf48cde8071afd
Prgwf <peter.wfgu@gmail.com>
2019-04-26 00:48:42 +0800

    implement mvcc insert, select.

--- src/include/mvcc/mvcc.h
+++ src/include/mvcc/mvcc.h
@@ -6,33 +6,32 @@
 #include "kv/kv.h"
 #include "kv/mock_kv.h"
 #include <memory>
+#include <string>
 #include <cstdint>
 
 
 namespace ctgfs {
 namespace mvcc {
 
-typedef std::uint64_t uint64;
-
 class MVCC {
   public:
-    virtual bool Insert(const string& key, const string& value, uint64 ts) = 0;
-    virtual bool Select(const string& key, const string& value, uint64 ts) = 0;
-    virtual bool Delete(const string& key, uint64 ts) = 0;
+    virtual bool Insert(const std::string& key, const std::string& value, int ts) = 0;
+    virtual bool Select(const std::string& key, std::string& value, int ts) = 0;
+    virtual bool Delete(const std::string& key, int ts) = 0;
 } ;
 
+using namespace kv;
 class SimpleMVCC : public MVCC {
- using ctgfs::kv;
 
  public:
   SimpleMVCC(const std::shared_ptr<KV> kv);
   ~SimpleMVCC();
-  bool Insert(const string& key, const string& value, uint64 ts) override;
-  bool Select(const string& key, const string& value, uint64 ts) override;
-  bool Delete(const string& key, uint64 ts) override;
+  bool Insert(const std::string& key, const std::string& value, int ts) override;
+  bool Select(const std::string& key, std::string& value, int ts) override;
+  bool Delete(const std::string& key, int ts) override;
 
  private:
-  std::shared_ptr<KV> kv_;
+  std::shared_ptr<KV> m_kv_;
 };
 
 }}
--- src/mvcc/mvcc.cc
+++ src/mvcc/mvcc.cc
@@ -1,11 +1,51 @@
 #include "mvcc/mvcc.h"
+#include <map>
+#include <climits>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <iostream>
 
+namespace ctgfs {
+namespace mvcc {
+namespace func {
+
+std::string ts_to_str(int t) {
+  return std::string((char*)(&t), sizeof(int));
+}
+
+int str_to_ts (std::string str) {
+  return *(int*)(str.c_str());
+}
+
+/* binary search to find the biggest value which is smaller than key or equal to key */
+/* {1, 3, 4, 7, 9}, key = 6 -> return 2(index) */
+int search_version(const std::vector< std::pair<int, int> >& vec, int key) {
+  /* if ret == -1, means the ts is smaller than all the timestamp in vector */
+  int ret = -1; 
+
+  int lb = 0, ub = vec.size();
+  while (lb <= ub) {
+    int mid = (lb + ub) / 2;
+
+    if (vec[mid].first <= key) {
+      ret = mid;
+      lb = mid + 1;
+    } else {
+      ub = mid - 1;
+    }
+  }
+
+  return ret;
+}
+
+}}}
 
 namespace ctgfs {
 namespace mvcc {
 
 SimpleMVCC::SimpleMVCC(const std::shared_ptr<KV> kv) 
-  : kv_(kv) {
+  : m_kv_(kv) {
 
 }
 
@@ -13,16 +53,60 @@ SimpleMVCC::~SimpleMVCC() {
 
 }
 
-bool SimpleMVCC::Insert(const string& key, const string& value, uint64 ts) {
+bool SimpleMVCC::Insert(const std::string& raw_key, const std::string& value, int ts) {
+  /* e.g. {key_2, value} , 2 is the timestamp*/
+  std::string key = raw_key + "_" + func::ts_to_str(ts);
 
+  m_kv_->Put(key, value);
+
+  return true;
 }
 
-bool SimpleMVCC::Select(const string& key, const string& value, uint64 ts) {
+bool SimpleMVCC::Select(const std::string& raw_key, std::string& value, int ts) {
+  std::map<std::string, std::string> data;
+  m_kv_->Query(raw_key, data);
 
+  if (data.empty())  {
+    return false;
+  }
+
+  std::vector< std::pair<int, int> > data_key;
+  std::vector< std::string > data_value;
+  int cnt = 0;
+  for (const auto & kv : data) {
+    /* get the index of '_' */
+    std::size_t pos_ = kv.first.find_last_of("_");
+
+    /* push {key, index} */
+    data_key.emplace_back(func::str_to_ts(kv.first.substr(pos_ + 1)), cnt++);
+
+    /* push value */
+    data_value.emplace_back(kv.second);
+  }
+
+  std::sort(data_key.begin(), data_key.end(), 
+    [](const std::pair<int, int>& x, const std::pair<int, int>& y) {
+      return x.first < y.first;
+  });
+
+  /* to find the latest timestamp which is smaller than ts */
+  int idx = func::search_version(data_key, ts);
+
+  /* the timestamp is smaller than any other.*/ 
+  if (idx < 0) {
+    value = "";
+    return false;
+  } 
+
+  value = data_value[data_key[idx].second];
+
+  return true;
 }
 
-bool SimpleMVCC::Delete(const string& key, uint64 ts) {
-
+bool SimpleMVCC::Delete(const std::string& raw_key, int ts) {
+  // TODO: implement this func.
+  return true;
 }
+
 
 }}
