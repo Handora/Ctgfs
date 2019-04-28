8bed75d3ac4b2af0c59f6a1c4806116daba1a193
Prgwf <peter.wfgu@gmail.com>
2019-04-28 22:09:36 +0800

    let's implement move!

---
+++ src/fs/extent_move.cc
@@ -0,0 +1,12 @@
+#include "fs/extent_server.h"
+#include "rpc/rpc.h"
+#include <vector>
+
+namespace ctgfs {
+namespace server {
+
+bool move(std::vector<std::string>& files, const std::string& src, std::string& dst) {
+
+}
+
+}}
