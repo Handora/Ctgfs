282da3092db8ea7f0b747e16b576267d01976e41
Handora <qcdsr970209@gmail.com>
2019-05-01 01:53:27 +0800

    add storage interface

---
+++ src/include/storage/interface.h
@@ -0,0 +1,49 @@
+// Authors: Chen Qian(qcdsr970209@gmail.com)
+
+#pragma once
+
+#include <util/status.h>
+#include <string>
+#include <storage/log.h>
+
+namespace ctgfs {
+namespace storage {
+
+using util::Status;
+using namespace util;
+using namespace std;
+
+template<typename KeyValue>
+class Iterator {
+ public:
+  Iterator() {};
+  virtual ~Iterator() {};
+  virtual bool HasNext() = 0;
+  virtual Status Next(KeyValue &key) = 0;
+};
+
+class Compactor {
+ public:
+  virtual Status compact() = 0;
+};
+
+class Flusher {
+ public:
+  virtual Status Flush(Iterator &it) = 0;
+};
+
+class Store {
+ public:
+  Store() {};
+  virtual ~Store() {};
+  virtual Status Init(const std::string& path) = 0;
+  virtual Status Start() = 0;
+  virtual Status Stop() = 0;
+  virtual Status Put(const std::string &key, const std::string &value);
+  virtual Status Get(const std::string &key, std::string &value);
+  virtual Status Delete(const std::string &key);
+  virtual Iterator<Log> Query(const std::string &Prefix);
+};
+
+}  // namespace wal
+}  // namespace ctgfs
