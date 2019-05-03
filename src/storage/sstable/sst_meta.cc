9ffe335f11fc590da742cfd0c55a476681b1ec1c
Handora <qcdsr970209@gmail.com>
2019-05-03 18:28:04 +0800

    fix storage meta read and write and log

--- src/storage/sstable/sst_meta.cc
+++ src/storage/sstable/sst_meta.cc
@@ -50,6 +50,92 @@ Status SSTMeta::Decode(const std::string &meta) {
   return ret;
 }
 
+Status SSTMeta::Write(int fd) {
+  Status ret = Status::OK();
+  uint64_t tmp_size = Size() + sizeof(uint64_t);
+
+  if (write(fd, &tmp_size, sizeof(uint64_t)) < 0) {
+    printf("write fail when write meta");
+    ret = Status::Corruption("write fail when write meta");
+  } else {
+    std::string tmp_str;
+
+    if (!(ret = Encode(tmp_str)).IsOK()) {
+      printf("Encode fail when write meta");
+    } else if (write(fd, tmp_str.c_str(), Size()) < 0) {
+      printf("write fail when write meta");
+      ret = Status::Corruption("write fail when write meta");
+    }
+  }
+
+  return ret;
+}
+
+Status SSTMeta::Write(int fd, uint64_t &writed) {
+  Status ret = Status::OK();
+  writed = Size() + sizeof(uint64_t);
+
+  if (write(fd, &writed, sizeof(uint64_t)) < 0) {
+    printf("write fail when write meta");
+    ret = Status::Corruption("write fail when write meta");
+  } else {
+    std::string tmp_str;
+
+    if (!(ret = Encode(tmp_str)).IsOK()) {
+      printf("Encode fail when write meta");
+    } else if (write(fd, tmp_str.c_str(), Size()) < 0) {
+      printf("write fail when write meta");
+      ret = Status::Corruption("write fail when write meta");
+    }
+  }
+
+  return ret;
+}
+
+Status SSTMeta::Read(int fd) {
+  Status ret = Status::OK();
+  uint64_t readed = 0;
+
+  if (read(fd, &readed, sizeof(uint64_t)) < 0) {
+    printf("read fail when read meta");
+    ret = Status::Corruption("read fail when read meta");
+  } else {
+    char *buf = new char[readed - sizeof(uint64_t)];
+    if (read(fd, buf, readed - sizeof(uint64_t)) < 0) {
+      printf("read fail when read meta");
+      ret = Status::Corruption("read fail when read meta");
+    } else if (!(ret = Decode(std::string(buf, readed - sizeof(uint64_t)))).IsOK()) {
+      printf("decode fail when read meta");
+    } else {
+      assert(Size() == readed - sizeof(uint64_t));
+    }
+  }
+
+  return ret;
+}
+
+Status SSTMeta::Read(int fd, uint64_t &readed) {
+  Status ret = Status::OK();
+
+  if (read(fd, &readed, sizeof(uint64_t)) < 0) {
+    printf("read fail when read meta");
+    ret = Status::Corruption("read fail when read meta");
+  } else {
+    char *buf = new char[readed - sizeof(uint64_t)];
+    if (read(fd, buf, readed - sizeof(uint64_t)) < 0) {
+      printf("read fail when read log");
+      ret = Status::Corruption("read fail when read meta");
+    } else if (!(ret = Decode(std::string(buf, readed - sizeof(uint64_t)))).IsOK()) {
+      printf("decode fail when read meta");
+    } else {
+      assert(Size() == readed - sizeof(uint64_t));
+    }
+  }
+
+  return ret;
+}
+
+
 }  // namespace sstable
 }  // namespace ctgfs
 
--- unittest/storage/test_sst_meta.cc
+++ unittest/storage/test_sst_meta.cc
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 #include <storage/sstable/sst_meta.h>
 #include <storage/log.h>
+#include <util/util.h>
 
 namespace ctgfs {
 namespace sstable {
@@ -22,6 +23,29 @@ TEST(SSTMetaTest, Basic) {
   std::string other_str;
   other.Encode(other_str);
   EXPECT_EQ(str, other_str);
+}
+
+TEST(SSTMetaTest, DiskIO) {
+  util::MyRemoveDirectoryRecursively("./tmp");
+  mkdir("./tmp", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+  Log l = Log(1, storage::Log::PUT, "key", "value");
+  SSTMeta meta(l, l.Size() + 8, 128);
+  int fd = open("tmp/test", O_CREAT | O_WRONLY, 0666);
+  EXPECT_TRUE(fd > 0);
+  uint64_t writed = 0;
+  meta.Write(fd, writed);
+  EXPECT_EQ(meta.Size() + sizeof(uint64_t), writed);
+  SSTMeta other;
+  close(fd);
+  fd = open("tmp/test", O_RDONLY);
+  EXPECT_TRUE(fd > 0);
+  uint64_t readed = 0;
+  other.Read(fd, readed);
+  EXPECT_EQ(writed, readed);
+  EXPECT_EQ(1, other.last_log_.lsn);
+  EXPECT_EQ("key", other.last_log_.key);
+  EXPECT_EQ("value", other.last_log_.value);
+  close(fd);
 }
 
 }  // namespace sstable
