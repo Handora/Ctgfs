d4e0534b5651bd5f2e1292a247c2a74d9c546671
Handora <qcdsr970209@gmail.com>
2019-05-14 01:11:23 +0800

    update mem wal

--- src/include/storage/memtable/wal.h
+++ src/include/storage/memtable/wal.h
@@ -21,22 +21,24 @@ class Wal {
  public:
   Wal();
   ~Wal();
-  Status Init(const std::string& dir);
+  Status Init(const std::string& dir, const std::function<void(const Log&)>& f);
   Status Recover();
   Status Start();
   Status Stop();
   Status LogToDisk(Log& log);
-  Status AppendToMLog(const Log& log);
-  Status SyncToDLog();
+  Status LogCurrent(uint64_t offset);
+  Status LogCurrent();
+  uint64_t GetDiskLogSize();
  private:
   Status LogToDiskWithoutLock(Log& log);
  private:
   bool init_;
-  std::vector<Log> mlog_;
   int dlog_;
   std::string dir_;
   std::string filename_;
+  std::string current_;
   std::mutex log_mu_;
+  std::function<void(const Log&)> func_;
 };
 
 }  // namespace wal
